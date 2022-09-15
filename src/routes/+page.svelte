19a419aa4b8ddb4c09fae2627ecb78c88ae79d5a
Régis Frias <regisfrias@gmail.com>
2022-09-15 16:16:30 +0300

    Replace some tabs with spaces.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
   import Input from '../lib/Input.svelte';
   import Sheet from '../lib/Sheet.svelte';
-	import '../app.css';
+  import '../app.css';
 
   import { tunes } from '../lib/tunes';
-	import { shuffle } from '../lib/utils';
+  import { shuffle } from '../lib/utils';
 
   let sheets = shuffle(tunes);
   let currentSheet = 0
 </script>
 
 <header>
-	<h1>Metal or Medal</h1>
-	<p>Is it metal? Is it not? Decide if the sheet below represents a piano reduction of a classical masterpiece or a transcription from hell.</p>
+  <h1>Metal or Medal</h1>
+  <p>Is it metal? Is it not? Decide if the sheet below represents a piano reduction of a classical masterpiece or a transcription from hell.</p>
 </header>
 
 <Sheet sheet={sheets[currentSheet]} />
 <Input bind:sheets={sheets} bind:currentSheet={currentSheet} />
 
 <style>
-	header {
-		max-width: 400px;
-		margin: auto;
-	}
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
 
-	h1 {
-		text-align: center;
-	}
+  h1 {
+    text-align: center;
+  }
 </style>
