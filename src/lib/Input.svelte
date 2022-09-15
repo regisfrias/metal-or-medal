cf127804f2512d7072deaa2cc82e9f9a65352e10
Régis Frias <regisfrias@gmail.com>
2022-09-15 17:44:17 +0300

    Replace tabs with spaces.

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Sheet from "./Sheet.svelte";
+  import Sheet from "./Sheet.svelte";
   import type { TuneType } from "./tunes";
   export let sheets: Array<TuneType>;
   export let currentSheet: number;
@@ -30,20 +30,26 @@
 <div class="input">
   {#if !done && currentSheet < sheets.length}
     <div class="buttons">
-      <button on:click={() => placeAnswer('METAL')} disabled={didAnswer} aria-label="Vote for metal">🤘</button>
-      <button on:click={() => placeAnswer('MEDAL')} disabled={didAnswer} aria-label="Vote for medal">🥇</button>
-      <button on:click={() => next()} disabled={!didAnswer} aria-label="Next">{sheets.length - 2 < currentSheet ? 'Finish' : 'Next'}</button>
+      <p>
+        <button class="choice" on:click={() => placeAnswer('METAL')} disabled={didAnswer} aria-label="Vote for metal">🤘</button>
+        <button class="choice" on:click={() => placeAnswer('MEDAL')} disabled={didAnswer} aria-label="Vote for medal">🥇</button>
+      </p>
+      <p>
+        <button on:click={() => next()} disabled={!didAnswer} aria-label="Next">{sheets.length - 2 < currentSheet ? 'Finish' : 'Next'}</button>
+      </p>
     </div>
   {/if}
 
   {#if didAnswer}
     <p>{answerRight ? 'Very good!': 'You have been fooled!'}</p>
-		<p>This piece is:</p>
+    <p>This piece is:</p>
     <p><strong>{ sheets[currentSheet].name }</strong>,<br>by { sheets[currentSheet].composer }</p>
   {/if}
 
-  <h2>Your score</h2>
-  <p>{ score }</p>
+  <div class="score">
+    <h2>Your score</h2>
+    <p>{ score }</p>
+  </div>
 </div>
 
 <style>
@@ -51,5 +57,13 @@
     max-width: 400px;
     margin: auto;
     text-align: center;
+  }
+
+  .input .buttons .choice {
+    font-size: 3rem;
+  }
+
+  .score {
+    border: 1px solid rgb(194, 194, 194);
   }
 </style>
