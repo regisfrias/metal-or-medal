93b27ad4b05fde72326860128918ac5d11faaffb
Régis Frias <regisfrias@gmail.com>
2022-10-16 21:57:51 +0300

    Add intro and buttons to home and faqe.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,30 @@
+
 <div class="wrapper">
+  <header><a class="btn" href="/">⌂</a></header>
+  <aside><a class="btn" href="/faqe">?</a></aside>
   <slot></slot>
 </div>
+
+<style>
+  header {
+    position: fixed;
+    top: 20px;
+    left: 20px;
+  }
+
+  aside {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+  }
+
+  a.btn {
+    text-decoration: none;
+    background-color: white;
+    padding: 20px;
+    display: block;
+    border-radius: 50%;
+    line-height: 0.5;
+    color: black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
   import Sheet from '../lib/Sheet.svelte';
   import '../app.css';
+
+  let intro = true;
+
+  function goPlay() {
+    intro = false;
+  }
 </script>
 
 <svelte:head>
 	<title>Metal or Medal</title>
 </svelte:head>
 
-<header>
-  <h1>Metal or Medal</h1>
-  <p>Is it metal? Is it not? Decide if the sheet below represents a piano reduction of a western classical music pantheon medal-winning masterpiece or a transcription from hell.</p>
-  <p>Wondering what this is? Read the <a href="/faqe">Faqe</a>.</p>
-</header>
+{#if intro}
+  <div class="intro">
+    <h1>Metal or Medal</h1>
+    <p>Is it metal? Is it not? Decide if the music sheet you see represents a piano reduction of a western classical music pantheon medal-winning masterpiece or a transcription from hell.</p>
+    <p>Wondering what this is? Read the <a href="/faqe">Faqe</a>.</p>
 
-<Sheet />
+    <button class="play" on:click={goPlay}>Play ></button>
+  </div>
+  {:else}
+  <Sheet />
+{/if}
 
 <style>
-  header {
+  .intro {
     max-width: 400px;
     margin: auto;
     padding: 1px;
